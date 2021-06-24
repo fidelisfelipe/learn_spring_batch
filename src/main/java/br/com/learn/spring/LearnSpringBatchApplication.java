32012d6cb1026229fa91db0e4df82f9e0bd38c83
Fidelis Felipe <atosfiel@gmail.com>
2021-06-24 13:34:33 -0300

    move properts bd

--- src/main/java/br/com/learn/spring/LearnSpringBatchApplication.java
+++ src/main/java/br/com/learn/spring/LearnSpringBatchApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnSpringBatchApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBatchApplication.class, args);
 	}
-
 }
