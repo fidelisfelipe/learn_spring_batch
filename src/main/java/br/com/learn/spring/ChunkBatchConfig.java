09722e0f8f142525170948537e80e45c8c895aa4
Fidelis Felipe <atosfiel@gmail.com>
2021-06-24 10:10:35 -0300

    remove imports not util

--- src/main/java/br/com/learn/spring/ChunkBatchConfig.java
+++ src/main/java/br/com/learn/spring/ChunkBatchConfig.java
@@ -1,27 +1,18 @@
 package br.com.learn.spring;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.function.FunctionItemProcessor;
 import org.springframework.batch.item.support.IteratorItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @EnableBatchProcessing
