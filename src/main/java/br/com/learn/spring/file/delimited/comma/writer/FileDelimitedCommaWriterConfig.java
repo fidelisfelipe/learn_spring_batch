b4a54b41d56d0e8204b799b6ef84000a17e6aa2e
Fidelis Felipe <atosfiel@gmail.com>
2021-07-14 21:41:03 -0300

    add write file delimited comma

--- src/main/java/br/com/learn/spring/file/delimited/comma/writer/FileDelimitedCommaWriterConfig.java
+++ src/main/java/br/com/learn/spring/file/delimited/comma/writer/FileDelimitedCommaWriterConfig.java
@@ -1,13 +1,26 @@
 package br.com.learn.spring.file.delimited.comma.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import br.com.learn.spring.comum.model.Client;
 
 @Configuration
 public class FileDelimitedCommaWriterConfig {
+	@StepScope
 	@Bean
-	public ItemWriter<br.com.learn.spring.comum.model.Client> fileDelimitedCommaWriter() {
-		return items -> items.forEach(System.out::println);
+	public FlatFileItemWriter<Client> fileDelimitedCommaWriter(@Value("#{jobParameters['fileDelimitedCommaOut']}") Resource file) {
+		return new FlatFileItemWriterBuilder<Client>()
+				.name("fileDelimitedCommaWriter")
+				.resource(file)
+				.delimited()
+				.delimiter(",")
+				.names("firstName","lastName","age","email")
+				.build();
 	}
 }
