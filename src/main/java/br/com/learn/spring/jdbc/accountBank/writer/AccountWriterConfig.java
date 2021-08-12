85243953a4476ce8a27b0e13fceae2de92bf7668
Fidelis Felipe <atosfiel@gmail.com>
2021-08-11 22:03:51 -0300

    add jdbc batch writer

--- src/main/java/br/com/learn/spring/jdbc/accountBank/writer/AccountWriterConfig.java
+++ src/main/java/br/com/learn/spring/jdbc/accountBank/writer/AccountWriterConfig.java
@@ -1,6 +1,14 @@
 package br.com.learn.spring.jdbc.accountBank.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +17,31 @@ import br.com.learn.spring.comum.model.Account;
 @Configuration
 public class AccountWriterConfig {
 
+	/**
+	 * For writer in lote in transactional
+	 * @param dataSource 
+	 * @return
+	 */
 	@Bean
-	public ItemWriter<Account> accountWriter() {
-		return items -> items.forEach(System.out::println);
+	public JdbcBatchItemWriter<Account> jdbcAccountWriter(@Qualifier("appDataSource") DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Account>()
+				.dataSource(dataSource)
+				.sql("INSERT INTO account(tipo,limite,client_id)VALUES(?,?,?)")
+				.itemPreparedStatementSetter(itemPreparedStatementSetter())
+				.build();
+	}
+
+	private ItemPreparedStatementSetter<Account> itemPreparedStatementSetter() {
+		return new ItemPreparedStatementSetter<Account>() {
+
+			@Override
+			public void setValues(Account item, PreparedStatement ps) throws SQLException {
+				ps.setString(1, item.getType().name());
+				ps.setDouble(2, item.getLimit());
+				ps.setString(3, item.getClientId());
+			}
+			
+		};
 	}
 	
 }
